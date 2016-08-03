8e178690a0175f53ad35926783a5c2f8d6f1c81d
Ratuvog <valkiriya.nd@gmail.com>
2016-08-03 12:16:56 +0400

    fix import

--- src/SmsPilotClient.js
+++ src/SmsPilotClient.js
@@ -1,5 +1,5 @@
 import request from 'request';
-import errorList from 'SmsPilotClientErrors';
+import errorList from './SmsPilotClientErrors';
 
 export default class SmsPilotClient {
   constructor(options = {}) {
